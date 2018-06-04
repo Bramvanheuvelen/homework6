b14950f00fb8d89ec9213af144bbd67fa12c3950
Bram van Heuvelen <bramvanheuvelen@gmail.com>
2018-06-04 21:37:18 +0200

    toppings locked after 3 choises

--- pizza/src/components/PizzaTop.js
+++ pizza/src/components/PizzaTop.js
@@ -9,7 +9,11 @@ class PizzaTop extends PureComponent {
     this.state = { value: '' };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+  
+  handleSubmit(e){
+    this.refs.form.reset();}
 
 handleChange = (e) => {
   if (e.target.checked) {
@@ -19,34 +23,40 @@ handleChange = (e) => {
   }
 }
 
-
 render() {
   return (
-    <form id="toppingform">
+    <form id="toppingform" onSubmit={this.handleSubmit}>
       <h1>Choose your 1, 2 or maximum 3 toppings</h1>
-      <input type="checkbox" value="Pineapple" onChange={this.handleChange} />
+      <input type="checkbox" value="Pineapple" onChange={this.handleChange} disabled={this.props.toppings.length === 3}/>
       <label for="Pineapple">Pineapple</label>
 
-      <input type="checkbox" value="Corn" onChange={this.handleChange} />
+      <input type="checkbox" value="Corn" onChange={this.handleChange} disabled={this.props.toppings.length === 3}/>
       <label for="Corn">Corn</label>
 
-      <input type="checkbox" value="Green olives" onChange={this.handleChange} />
+      <input type="checkbox" value="Green olives" onChange={this.handleChange} disabled={this.props.toppings.length === 3} />
       <label for="Green Olives">Green olives</label>
 
-      <input type="checkbox" value="Red onion" onChange={this.handleChange} />
+      <input type="checkbox" value="Red onion" onChange={this.handleChange} disabled={this.props.toppings.length === 3}/>
       <label for="Red Onion">Red onion</label>
 
-      <input type="checkbox" value="Spinach" onChange={this.handleChange} />
+      <input type="checkbox" value="Spinach" onChange={this.handleChange} disabled={this.props.toppings.length === 3}/>
       <label for="Spinach">Spinach</label>
 
-      <input type="checkbox" value="Cherry tomatoes" onChange={this.handleChange} />
+      <input type="checkbox" value="Cherry tomatoes" onChange={this.handleChange} disabled={this.props.toppings.length === 3} />
       <label for="Cherry Tomatoes">Cherry tomatoes</label>
 
-      <input type="checkbox" value="Chicken" onChange={this.handleChange} />
+      <input type="checkbox" value="Chicken" onChange={this.handleChange} disabled={this.props.toppings.length === 3}/>
       <label for="Chicken">chicken</label>
+      <input type="submit" value="Reset" />
     </form>
   )
 }
 }
 
-export default connect(null, { chooseTopping, deleteTopping })(PizzaTop)
+function mapStateToProps(state) {
+  return {
+    toppings: state.toppings
+  }
+}
+
+export default connect(mapStateToProps, { chooseTopping, deleteTopping })(PizzaTop)
--- pizza/src/reducers/toppingchoice.js
+++ pizza/src/reducers/toppingchoice.js
@@ -3,12 +3,12 @@ import { CHOOSE_TOPPING, DELETE_TOPPING } from '../actions/index'
 export default function (state = [], action = {}) {
   switch (action.type) {
   case CHOOSE_TOPPING:
-    if (state.length < 3) {
+    // if (state.length < 3) {
       return state.concat(action.payload)
-    } else {
-      window.alert('3 toppings is the max')
-      return state
-    }
+    // } else {
+    //   window.alert('3 toppings is the max')
+    //   return state
+    // }
 
   case DELETE_TOPPING:
     return state.filter(topping => topping !== action.payload)
